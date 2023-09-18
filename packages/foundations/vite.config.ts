087e9ee4c11afb460fd088c84842bd85b0752098
fathurhidayat <fathur.hidayat@amartha.com>
2023-09-19 06:15:15 +0700

    fix: fix glob

--- packages/foundations/vite.config.ts
+++ packages/foundations/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import { extname, relative, resolve } from "path";
 import { fileURLToPath } from "node:url";
-import { glob } from "glob";
+import glob from "glob";
 import react from "@vitejs/plugin-react";
 import dts from "vite-plugin-dts";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
